246780e72be403042bd83559551701d410c36674
dadoux <streetlascars@gmail.com>
2017-09-26 10:30:32 +0200

    Mise en place environement de dev

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Clean Blog - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/clean-blog.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Formation testing</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <!-- 
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="post.html">Sample Post</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+         -->
+
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Espace Doc</h1>
+              <span class="subheading">Platforme de partage de fichiers</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview">
+              <h2 id="page_heading">Documents à télecharger </h2>
+          
+              <div class="media doc_items">
+              <div class="media-left media-middle">
+                <a href="#">
+                  <img class="media-object" src="img/file_icons/_blank.png" width="100" alt="...">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading"><a href="#">aligned media</a> </h4>
+                <p class="post-meta">Posted by
+                <a href="#">Start Bootstrap</a>on August 24, 2017 | télechargé (2) fois</p>              
+                <p>Cras sit amet nibh libero, in gravida nulla.Nulla vel metus.</p>
+              </div>
+            </div>
+             <div class="media doc_items">
+              <div class="media-left media-middle">
+                <a href="#">
+                  <img class="media-object" src="img/file_icons/pdf.png" width="100" alt="...">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading"><a href="#">aligned media on de the script for none paradise technique_frgemontagouxsy</a> </h4>
+                <p class="post-meta">Posted by
+                 <a href="#">Start Bootstrap</a>on August 24, 2017 | télechargé (2) fois
+               </p>   
+                <p>Cras sit amet nibh libero, in gravida nulla.
+                  Nulla vel metus.</p>
+              </div>
+            </div>
+                  
+            
+
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-secondary float-right" href="#">Older Posts &rarr;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                <a href="#" class="btn btn-secondary float-right" href="#">
+                  Ajouter un documents
+                </a>
+              </li>
+              
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; Your Website 2017</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/clean-blog.min.js"></script>
+
+  </body>
+
+</html>
